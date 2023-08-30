cc850cde0713503697986dc19dbfee84f22e7e25
Guilherme Sanches Romano <gui.romano.dev@gmail.com>
2023-08-29 23:19:11 -0300

    Create Exercício7.py

---
+++ Listas de exercício/Lista1/Exercício7.py
@@ -0,0 +1,10 @@
+#Fornecido um número inteiro n (n ≥ 0), exibir a raiz quadrada desse valor, para tanto utilize o operador
+#de potência (∗∗).
+
+n = int(input('Forneça um número que queira a raiz quadrada:'))
+
+c = n**(1/2)
+
+print('A raiz quadrada de %d é %d.' %(n,c))
+
+
